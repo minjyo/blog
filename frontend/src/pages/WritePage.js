d873906155c967f79e3e46c5ada44b14cbf4eaac
minjyo <minjyo8823@gmail.com>
2021-11-04 18:25:44 +0900

    feat: add editor container and set event handler to quill

--- frontend/src/pages/WritePage.js
+++ frontend/src/pages/WritePage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Editor from '../components/write/Editor';
 import Responsive from 'components/common/Responsive';
 import WriteActionButtonS from 'components/write/WriteActionButtons';
+import EditorContainer from 'containers/write/EditorContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
-      <Editor />
+      <EditorContainer />
       <WriteActionButtonS />
     </Responsive>
   );
