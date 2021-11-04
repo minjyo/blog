ed81c67391f0b74cda98b8149a9950efb48c4723
minjyo <minjyo8823@gmail.com>
2021-11-04 15:05:09 +0900

    feat: add redux module, rootSaga and apply redux-saga middleware

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -1,10 +1,15 @@
 import { combineReducers } from 'redux';
-import post from './post';
+import post, { postSaga } from './post';
 import loading from './loading';
+import { all } from 'redux-saga/effects';
 
 const rootReducer = combineReducers({
   post,
   loading,
 });
+
+export function* rootSaga() {
+  yield all([postSaga()]);
+}
 
 export default rootReducer;
--- frontend/src/modules/post.js
+++ frontend/src/modules/post.js
@@ -1,10 +1,23 @@
 import { createAction, handleActions } from 'redux-actions';
+import { takeLatest } from 'redux-saga/effects';
+import {
+  createRequestActionTypes,
+  createRequestSaga,
+} from 'lib/createRequestSaga';
+import * as postAPI from 'lib/api/post';
 
 // DUCK 패턴: 액션 타입 + 액션 생성 함수 + 리듀서 -> 리덕스
 
 const SAMPLE_ACTION = 'post/SAMPLE_ACTION';
 
 export const sampleAction = createAction(SAMPLE_ACTION);
+
+
+// 사가 생성
+//const sampleSaga = createRequestSaga(SAMPLE_ACTION, postAPI.sample);
+export function* postSaga() {
+  // yield takeLatest(SAMPLE_ACTION, sampleSaga);
+}
 
 const initialState = {};
 
