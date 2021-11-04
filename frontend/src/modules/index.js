eeffca108f94d163a70ff8735b22aad8ae0e3e23
minjyo <minjyo8823@gmail.com>
2021-11-04 18:09:38 +0900

    feat: add write redux module

--- frontend/src/modules/index.js
+++ frontend/src/modules/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import post, { postSaga } from './post';
 import loading from './loading';
 import { all } from 'redux-saga/effects';
+import write from './write';
 
 const rootReducer = combineReducers({
   post,
   loading,
+  write,
 });
 
 export function* rootSaga() {
---
+++ frontend/src/modules/write.js
@@ -0,0 +1,25 @@
+import { createAction, handleActions } from 'redux-actions';
+
+const INITIALIZE = 'write/INITIALIZE';
+const CHANGE_FIELD = 'write/CHANGE_FIELD';
+
+export const initialize = createAction(INITIALIZE);
+export const changeField = createAction(CHANGE_FIELD, ({ key, value }) => ({
+  key,
+  value,
+}));
+
+const initialState = {};
+
+const write = handleActions(
+  {
+    [INITIALIZE]: (state) => initialState,
+    [CHANGE_FIELD]: (state, { payload: { key, value } }) => ({
+      ...state,
+      [key]: value,
+    }),
+  },
+  initialState,
+);
+
+export default write;
