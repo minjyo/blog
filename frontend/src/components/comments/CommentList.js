c8772bec3af77703863daf74a2540dd8664ed05d
minjyo <minjyo8823@gmail.com>
2021-11-05 03:17:11 +0900

    feat: add commentList component

---
+++ frontend/src/components/comments/CommentList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import palette from 'lib/styles/palette';
+import Responsive from 'components/common/Responsive';
+
+const CommentListBlock = styled(Responsive)`
+  margin-top: 3rem;
+`;
+const CommentItemBlock = styled.div`
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+
+  & + & {
+    border-top: 1px solid ${palette.gray[2]};
+  }
+
+  h4 {
+    font-size: 1.5rem;
+    margin-bottom: 0;
+  }
+  p {
+    margin-top: 1rem;
+  }
+`;
+
+const CommentItem = ({ comment }) => {
+  const { username, body } = comment;
+
+  return (
+    <CommentItemBlock>
+      <h3>{username}</h3>
+      <p>{body}</p>
+    </CommentItemBlock>
+  );
+};
+
+const CommentItemList = ({ comments, loading, error }) => {
+  if (error) {
+    return <CommentListBlock>오류 발생</CommentListBlock>;
+  }
+
+  return (
+    <CommentListBlock>
+      {!loading && comments && (
+        <div>
+          {comments.map((comment) => (
+            <CommentItem comment={comment} key={comment._id}></CommentItem>
+          ))}
+        </div>
+      )}
+    </CommentListBlock>
+  );
+};
+
+export default CommentItemList;
--- frontend/src/containers/post/PostViewerContainer.js
+++ frontend/src/containers/post/PostViewerContainer.js
@@ -6,15 +6,19 @@ import PostViewer from 'components/post/PostViewer';
 import PostActionButtons from 'components/post/PostActionButtons';
 import { setOriginalPost } from 'modules/write';
 import { removePost } from 'lib/api/posts';
+import CommentItemList from 'components/comments/CommentList';
 
 const PostViewerContainer = ({ match, history }) => {
   const { postId } = match.params;
   const dispatch = useDispatch();
-  const { post, error, loading } = useSelector(({ post, loading }) => ({
-    post: post.post,
-    error: post.error,
-    loading: loading['post/READ_POST'],
-  }));
+  const { post, error, loading, comments } = useSelector(
+    ({ post, loading }) => ({
+      post: post.post,
+      error: post.error,
+      loading: loading['post/READ_POST'],
+      comments: post.comments,
+    }),
+  );
 
   useEffect(() => {
     dispatch(readPost(postId));
@@ -39,12 +43,17 @@ const PostViewerContainer = ({ match, history }) => {
   };
 
   return (
-    <PostViewer
-      post={post}
-      loading={loading}
-      error={error}
-      actionButtons={<PostActionButtons onEdit={onEdit} onRemove={onRemove} />}
-    ></PostViewer>
+    <>
+      <PostViewer
+        post={post}
+        loading={loading}
+        error={error}
+        actionButtons={
+          <PostActionButtons onEdit={onEdit} onRemove={onRemove} />
+        }
+      ></PostViewer>
+      <CommentItemList comments={comments} loading={loading} error={error} />
+    </>
   );
 };
 
